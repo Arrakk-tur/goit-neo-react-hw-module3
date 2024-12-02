6b756f24fe77192886a7ed5258cfa2e71204d57e
Kostiantyn Skliar <skljar.k@gmail.com>
2024-12-03 01:22:44 +0200

    Step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
 import { useState } from "react";
 import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
 
 function App() {
-  const [contacts, setContacts] = useState([
+  const contacts = [
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  ];
 
-  const deleteContact = (contactId) => {
-    setContacts(contacts.filter((contact) => contact.id !== contactId));
+  const [filter, setFilter] = useState("");
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   };
+
+  const getFilteredContacts = () => {
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  // const deleteContact = (contactId) => {
+  //   setContacts(contacts.filter((contact) => contact.id !== contactId));
+  // };
   return (
     <div>
       <h1>Phonebook</h1>
-      {/* <ContactForm />
-      <SearchBox /> */}
-      <ContactList contacts={contacts} onDeleteContact={deleteContact} />
+      {/* <ContactForm /> */}
+      <SearchBox value={filter} onChange={handleFilterChange} />
+      <ContactList contacts={getFilteredContacts()} />
     </div>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,11 @@
 import Contact from "./Contact";
 import styles from "./ContactList.module.css";
 
-const ContactList = ({ contacts, onDeleteContact }) => {
+const ContactList = ({ contacts }) => {
   return (
     <ul className={styles.contactList}>
       {contacts.map(({ id, name, number }) => (
-        <Contact
-          key={id}
-          name={name}
-          number={number}
-          onDelete={() => onDeleteContact(id)}
-        />
+        <Contact key={id} name={name} number={number} />
       ))}
     </ul>
   );
