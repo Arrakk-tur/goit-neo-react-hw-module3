ec89845e0819b2b4d49ba3b2166bc655eb61fa1b
Kostiantyn Skliar <skljar.k@gmail.com>
2024-12-05 01:58:21 +0200

    Step 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ContactList from "./components/ContactList/ContactList";
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const LOCAL_STORAGE_KEY = "contacts";
+
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return savedContacts
+      ? JSON.parse(savedContacts)
+      : [
+          // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
+  });
 
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
