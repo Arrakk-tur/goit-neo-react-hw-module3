0dd1a659f69816c11fad6e3e3f8e6dfa49931c37
Kostiantyn Skliar <skljar.k@gmail.com>
2024-12-04 02:14:37 +0200

    Step 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import ContactList from "./components/ContactList/ContactList";
+import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
 
 function App() {
-  const contacts = [
+  const [contacts, setContacts] = useState([
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ];
+  ]);
 
   const [filter, setFilter] = useState("");
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
+  };
+
+  const handleAddContact = (newContact) => {
+    setContacts((prevContacts) => [newContact, ...prevContacts]);
+    console.log(newContact);
   };
 
   const getFilteredContacts = () => {
@@ -23,13 +29,10 @@ function App() {
     );
   };
 
-  // const deleteContact = (contactId) => {
-  //   setContacts(contacts.filter((contact) => contact.id !== contactId));
-  // };
   return (
     <div>
       <h1>Phonebook</h1>
-      {/* <ContactForm /> */}
+      <ContactForm onAddContact={handleAddContact} />
       <SearchBox value={filter} onChange={handleFilterChange} />
       <ContactList contacts={getFilteredContacts()} />
     </div>
